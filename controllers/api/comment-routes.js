8f2661b6b7c105ca04c17b66eb70a92557332d66
David Jover <david.jover59@gmail.com>
2023-03-08 22:04:53 -0500

    I just uploaded what I had done.

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const {
+    User,
+    Post,
+    Comment
+} = require('../../models');
+
+
+//Get all comments
+router.get("/", (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//Create a comment
+router.post('/', (req, res) => {
+    if (req.session) {
+        Comment.create({
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                user_id: req.session.user_id
+            })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+});
+
+// router.post("/", (req, res) => {
+//     Comment.create({
+//             comment_text: req.body.comment_text,
+//             post_id: req.body.post_id,
+//             user_id: req.body.user_id,
+//         })
+//         .then((dbCommentData) => res.json(dbCommentData))
+//         .catch((err) => {
+//             console.log(err);
+//             res.status(400).json(err);
+//         });
+// });
+
+
+module.exports = router;
